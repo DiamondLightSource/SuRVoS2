ef19da194d8ac200b6ee1cab56da44198723e369
Pennington <avery.pennington@diamond.ac.uk>
2020-08-26 12:38:23 +0100

    config cleanup

--- survos2/frontend/project.py
+++ survos2/frontend/project.py
@@ -2,92 +2,19 @@ import json
 import argparse
 import numpy as np
 import pyqtgraph as pg
-from pyqtgraph.Qt import QtCore, QtGui
+import sys
+import signal
 
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 import pyqtgraph.parametertree.parameterTypes as pTypes
 from pyqtgraph.parametertree import Parameter, ParameterTree, ParameterItem, registerParameterType
-
-app = QtGui.QApplication([])
-
 from qtpy.QtWidgets import QLabel, QTabWidget, QVBoxLayout, QPushButton, QWidget
 
-
-class ParameterTreeWidget:
-    def __init__(self, params):
-
-        p = setup_parameter_tree(params)
-        t = ParameterTree()
-        t.setParameters(p, showTop=False)
-        t.setWindowTitle('pyqtgraph example: Parameter Tree')
-
-        w = QtGui.QWidget()
-        layout = QtGui.QGridLayout()
-        w.setLayout(layout)
-        layout.addWidget(t, 1, 0, 1, 1)
-        w.show()
-        w.resize(800, 800)
-
-        s = p.saveState()
-        p.restoreState(s)
-
-        self.w = w
-
-
-def parameter_tree_change(param, changes):
-    print("tree changes:")
-
-    for param, change, data in changes:
-        path = scfg.p.childPath(param)
-
-        if path is not None:
-            childName = '.'.join(path)
-        else:
-            childName = param.name()
-
-        sibs = param.parent().children()
-
-        logger.debug(f"Path: {path}")
-        logger.debug(f"Parent: {param.parent}")
-        logger.debug(f"Siblings: {sibs}")
-        logger.debug(f"Value: {param.value}")
-
-        if childName == "MainGroup.Integer":
-            logger.debug(f"Setting value: {data}")
-            scfg.marker_size = int(data)
-
-        print('  parameter: %s' % childName)
-        print('  change:    %s' % change)
-        print('  data:      %s' % str(data))
-        print('\n')
-
-
-def setup_parameter_tree(p):
-    p.sigTreeStateChanged.connect(parameter_tree_change)  # callback 1
-
-    def valueChanging(param, value):
-        print("Value: %s\n %s" % (param, value))
-
-    for child in p.children():
-        child.sigValueChanging.connect(valueChanging)  # callback 2
-        
-        for ch2 in child.children():
-            ch2.sigValueChanging.connect(valueChanging)
-
-    def save():
-        global state
-        state = p.saveState()
-
-    def restore():
-        global state
-        add = p['Save/Restore functionality', 'Restore State', 'Add missing items']
-        rem = p['Save/Restore functionality', 'Restore State', 'Remove extra items']
-        p.restoreState(state, addChildren=add, removeChildren=rem)
-
-    p.param('Save/Restore functionality', 'Save State').sigActivated.connect(save)
-    p.param('Save/Restore functionality', 'Restore State').sigActivated.connect(restore)
-
-    return p
-
+from loguru import logger
+fmt = "{level} - {name} - {message}"
+logger.remove() # remove default logger
+logger.add(sys.stderr, level="DEBUG", format=fmt)  #minimal stderr logger
 
 
 params = [
@@ -117,23 +44,6 @@ params = [
 ]
 
 
-ptree_init3 = [
-    {'name': 'Name', 'type': 'group', 'children': [
-        {'name': 'Some int', 'type': 'int', 'value':10},
-        {'name': 'Some float', 'type': 'float', 'value': 10.5, 'step': 0.1},
-        {'name': 'String of some kind', 'type': 'str', 'value': "Hi"},
-        {'name': 'List', 'type': 'list', 'values': [1,2,3], 'value': 2},
-    ]},
-        {'name': 'Save/Restore functionality', 'type': 'group', 'children': [
-        {'name': 'Save State', 'type': 'action'},
-        {'name': 'Restore State', 'type': 'action', 'children': [
-            {'name': 'Add missing items', 'type': 'bool', 'value': True},
-            {'name': 'Remove extra items', 'type': 'bool', 'value': True},
-        ]},
-    ]},
-
-]
-
 ptree_init2 = [{'name': 'Name',
   'type': 'group',
   'children': [{'name': 'datasets_dir',
@@ -154,14 +64,64 @@ ptree_init2 = [{'name': 'Name',
     'values': [0, 64, 100, 228, 100, 228]}]}]
 
 
-def setup_ptree_params():
-    with open('./projects/brain/ws_brain.json') as project_file:    
-            wparams = json.load(project_file)
+
+def setup_ptree_params(p):
+    def parameter_tree_change(param, changes):
+        print("tree changes:")
+
+        for param, change, data in changes:
+            path = p.childPath(param)
+
+            if path is not None:
+                childName = '.'.join(path)
+            else:
+                childName = param.name()
+
+            sibs = param.parent().children()
+
+            logger.debug(f"Path: {path}")
+            logger.debug(f"Parent: {param.parent}")
+            logger.debug(f"Siblings: {sibs}")
+            logger.debug(f"Value: {param.value}")
+
+            logger.debug('  parameter: %s' % childName)
+            logger.debug('  change:    %s' % change)
+            logger.debug('  data:      %s' % str(data))
+
+
+    p.sigTreeStateChanged.connect(parameter_tree_change)  # callback 1
+
+    def valueChanging(param, value):
+        print("Value: %s\n %s" % (param, value))
+
+    for child in p.children():
+        child.sigValueChanging.connect(valueChanging)  # callback 2
+        
+        for ch2 in child.children():
+            ch2.sigValueChanging.connect(valueChanging)
+
+    def save():
+        global state
+        state = p.saveState()
+
+    def restore():
+        global state
+        add = p['Save/Restore functionality', 'Restore State', 'Add missing items']
+        rem = p['Save/Restore functionality', 'Restore State', 'Remove extra items']
+        p.restoreState(state, addChildren=add, removeChildren=rem)
+
+    #p.param('Save/Restore functionality', 'Save State').sigActivated.connect(save)
+    #p.param('Save/Restore functionality', 'Restore State').sigActivated.connect(restore)
+
+    return p
+
+
+def dict_to_params(param_dict):
 
     ptree_param_dicts = []
 
-    for key in wparams.keys():
-        entry = wparams[key]
+    for key in param_dict.keys():
+        entry = param_dict[key]
         print(type(entry))
         if type(entry) == str:
             d = {'name': key, 'type': 'str' , 'value' : entry}
@@ -182,84 +142,51 @@ def setup_ptree_params():
     return ptree_init
 
 
-ptree_init = setup_ptree_params()
 
-p = Parameter.create(name='ptree_init', type='group', children=ptree_init)
-
-def change(param, changes):
-    print("tree changes:")
-    for param, change, data in changes:
-        path = p.childPath(param)
-        if path is not None:
-            childName = '.'.join(path)
-        else:
-            childName = param.name()
-        print('  parameter: %s'% childName)
-        print('  change:    %s'% change)
-        print('  data:      %s'% str(data))
-        print('  ----------')
-    
-p.sigTreeStateChanged.connect(change)
-
-def valueChanging(param, value):
-    print("Value changing (not finalized): %s %s" % (param, value))
-    
-# Too lazy for recursion:
-for child in p.children():
-    child.sigValueChanging.connect(valueChanging)
-    for ch2 in child.children():
-        ch2.sigValueChanging.connect(valueChanging)
-        
+def init_ptree(wparams):
+    ptree_init = dict_to_params(wparams)
+    parameters = Parameter.create(name='ptree_init', type='group', children=ptree_init)
+    params = setup_ptree_params(parameters)
+    ptree = ParameterTree()
+    ptree.setParameters(params, showTop=False)    
+    print("Ptree created")
+    return ptree
 
 
-ptree = ParameterTree()
-ptree.setParameters(p, showTop=False)
-ptree.setWindowTitle('pyqtgraph example: Parameter Tree')
-
-
-win = QtGui.QWidget()
-layout = QVBoxLayout()
-win.setLayout(layout)
-
-
-tabwidget = QTabWidget()
-tab1 = QWidget()
-tab2 = QWidget()
-
-tabwidget.addTab(tab1, "Project File")
-tabwidget.addTab(tab2, "Run")
-
-tab1.layout = QVBoxLayout()
-tab1.setLayout(tab1.layout)
-tab1.layout.addWidget(ptree)
-
-button_run = QPushButton("Run")
-
-tab2.layout = QVBoxLayout()
-tab2.setLayout(tab2.layout)
-tab2.layout.addWidget(button_run)
-
-layout.addWidget(tabwidget)
-
-win.show()
-win.resize(800,800)
-
-
-def init():
+def init(layout):
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--project_file', default='./projects/brain/ws_brain.json')
     args = parser.parse_args()
     
     with open(args.project_file) as project_file:    
-            wparams = json.load(project_file)
-            
+        wparams = json.load(project_file)
+
+    ptree = init_ptree(wparams)
+
+    tabwidget = QTabWidget()
+    tab1 = QWidget()
+    tab2 = QWidget()
+
+    tabwidget.addTab(tab1, "Project File")
+
+    button_run = QPushButton("Run")
+
+    tab1.layout = QVBoxLayout()
+    tab1.setLayout(tab1.layout)
+    tab1.layout.addWidget(ptree)
+    tab1.layout.addWidget(button_run)
+
+
+    layout.addWidget(tabwidget)
+
 
 if __name__ == "__main__":
-    import sys
-    init()
-
-    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_()
-
-
-
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    
+    app = QApplication([])
+    window = QWidget()
+    layout = QVBoxLayout()
+    window.setLayout(layout)
+    init(layout)
+    window.show()
+    app.exec_()
