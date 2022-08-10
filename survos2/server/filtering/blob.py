Fix feature normalization issue.

survos2/server/filtering/blob.py
@@ -148,8 +148,10 @@ def hessian_eigvals(data, sigma, correct=False):
 
     img: np.ndarray = kornia.tensor_to_image(e)
     img = np.transpose(img, (0, 3, 1, 2))
-
-    return np.nan_to_num(img)
+    img = np.nan_to_num(img)
+    img -= np.min(img)
+    img /= np.max(img)
+    return img
 
 
 def hessian_eigvals_image(data, sigma, correct=False):
@@ -188,8 +190,10 @@ def hessian_eigvals_image(data, sigma, correct=False):
     img: np.ndarray = kornia.tensor_to_image(e)
     img = np.transpose(img, (0, 3, 1, 2))
     img = img[:, :, :, 0]  # return primay eigenvalue
-
-    return np.nan_to_num(img)
+    img = np.nan_to_num(img)
+    img -= np.min(img)
+    img /= np.max(img)
+    return img
 
 
 def hessian_eigvals_p(data, sigma, correct=False):
@@ -288,6 +292,9 @@ def compute_structure_tensor_determinant(data, sigma=1):
         + Sxz * (Sxy * Syz - Syy * Sxz)
     )
 
+    determinant -= np.min(determinant)
+    determinant /= np.max(determinant)
+
     return determinant
 
 
@@ -380,5 +387,10 @@ def compute_frangi(
             tmp[e3 > 0] = 0
 
         filtered_array[i] = np.nan_to_num(tmp)
+    
+    img = np.max(filtered_array, axis=0)
+    img -= np.min(img)
+    img /= np.max(img)
+    return img 
+    
 
-    return np.max(filtered_array, axis=0)

survos2/server/filtering/edge.py
@@ -43,6 +43,9 @@ def ndimage_laplacian(img, kernel_size=1.0):
     img = np.nan_to_num(img)
     img = ndimage.laplace(gaussian(img, kernel_size))
 
+    img = img - np.min(img)
+    img = img / np.max(img)
+
     return img
 
 
@@ -72,6 +75,9 @@ def spatial_gradient_3d(vol_gray: np.ndarray, dim=0) -> np.ndarray:
     result_arr: np.ndarray = kornia.tensor_to_image(result.float())
     logger.debug(f"Calculated gradient of shape {result_arr.shape}")
 
+    result_arr -= np.min(result_arr)
+    result_arr /= np.max(result_arr)
+    
     return result_arr
 
 
@@ -91,8 +97,12 @@ def laplacian(img: np.ndarray, kernel_size) -> np.ndarray:
         kernel_size += 1
     laplacian: torch.Tensor = kornia.filters.laplacian(img_clean_t, kernel_size=kernel_size)
     laplacian_img: np.ndarray = kornia.tensor_to_image(laplacian.float())
-    return np.nan_to_num(laplacian_img)
 
+    laplacian_img = np.nan_to_num(laplacian_img)
+    laplacian_img -= np.min(laplacian_img)
+    laplacian_img /= np.max(laplacian_img)
+
+    return laplacian_img
 
 def compute_difference_gaussians(
     data, sigma, sigma_ratio, threshold=False, dark_response=False
@@ -132,4 +142,8 @@ def compute_difference_gaussians(
 
     response = np.nan_to_num(response)
 
+    response -= np.min(response)
+    response /= np.max(response)
+
     return response
+

survos2/server/filtering/wavelet.py
@@ -32,12 +32,10 @@ def wavelet3d(I, level, wavelet="sym3", threshold=64.0, hard=True):
 def wavelet(I, level, wavelet="db3", threshold=64.0, hard=True):
     mode = "symmetric"
     result = np.zeros_like(I)
-    print(I.shape)
     for i in range(I.shape[0]):
         s = np.zeros_like(result[i,:])
         s[0:I.shape[1],0:I.shape[2]] = I[i,:]
         arr = np.float32(s)
-        print(f"arr.shape {arr.shape}")
         coeffs = wavedec2(arr, wavelet=wavelet, level=7)
         coeffs_H = list(coeffs)
 
@@ -63,8 +61,11 @@ def wavelet(I, level, wavelet="db3", threshold=64.0, hard=True):
         #     coeffs_H[idx][1] = np.sign(coeffs_H[idx][1]) * np.abs(coeffs_H[idx][1] - threshold)
 
         arr_rec = waverec2(coeffs_H, wavelet=wavelet)
-        print(f"arr_rec {arr_rec.shape}")
         result[i,0:I.shape[1],0:I.shape[2]] = arr_rec[0:I.shape[1],0:I.shape[2]].copy()
 
+    result -= np.min(result)
+    result /= np.max(result)
+
     return result
+
 